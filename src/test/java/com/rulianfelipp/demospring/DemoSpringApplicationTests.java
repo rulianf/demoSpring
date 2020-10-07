c1610b0e523072d4b74e82b31e3eae5427177fdd
Rulian Felipp <rulianf@gmail.com>
2020-10-07 15:06:08 -0300

    Criação do Projeto

---
+++ src/test/java/com/rulianfelipp/demospring/DemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rulianfelipp.demospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
